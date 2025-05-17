d9cb7aab415cbb52e43f7cbd07bfeb09f33a64db
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2025-05-17 02:24:38 -0500

    "feat(app): Integrate authentication state and Login/Logout buttons in AppComponent"

--- angular-ba-frontend/src/app/app.component.ts
+++ angular-ba-frontend/src/app/app.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, inject} from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { AuthService } from '@auth0/auth0-angular';
+import { LoginButtonComponent } from './shared/components/login-button/login-button.component';
+import { LogoutButtonComponent } from './shared/components/logout-button/logout-button.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, LoginButtonComponent, LogoutButtonComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-ba-frontend';
+
+  public auth = inject(AuthService);
+
+  mobileMenuOpen = false;
+
+  toggleMobileMenu() {
+    this.mobileMenuOpen = !this.mobileMenuOpen;
+  }
+
+  getYear() {
+    return new Date().getFullYear();
+  }
 }
