3c46cbf1ade85322c74754194d8421056ceae7a4
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2025-05-17 02:22:24 -0500

    feat(auth): Install Auth0 Angular SDK and setup initial provider configuration

--- angular-ba-frontend/src/app/app.config.ts
+++ angular-ba-frontend/src/app/app.config.ts
@@ -1,8 +1,40 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
+import {AuthHttpInterceptor, provideAuth0} from '@auth0/auth0-angular';
 
 import { routes } from './app.routes';
+import {provideAnimations} from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi()),
+    provideAuth0({
+      domain: 'dev-z0d7qop2x7xnoumm.us.auth0.com',
+      clientId: 'uUWtRtWDkqqaghN35R3EkvnTx4OKzwM3',
+      authorizationParams: {
+        redirect_uri: window.location.origin,
+        audience: 'https.api.bankapp.com',
+      },
+      httpInterceptor: {
+        allowedList: [
+          {
+            uri: '*',
+            tokenOptions: {
+              authorizationParams: {
+                audience: 'https.api.bankapp.com'
+              }
+            }
+          }
+        ]
+      }
+    }),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthHttpInterceptor,
+      multi: true
+    },
+    provideAnimations()
+  ]
 };
