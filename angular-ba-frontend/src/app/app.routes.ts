ce4603b9a456cc4348db891c9ad3a2ccb026a408
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2025-05-17 02:25:52 -0500

    "feat(routing): Implement application routing with AuthGuard for protected routes"

--- angular-ba-frontend/src/app/app.routes.ts
+++ angular-ba-frontend/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from '@auth0/auth0-angular';
+import { UserProfileComponent } from './features/profile/user-profile/user-profile.component';
+import {HomeComponent} from './features/home/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '',
+    component: HomeComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'profile',
+    component: UserProfileComponent,
+    canActivate: [AuthGuard],
+  }
+];
